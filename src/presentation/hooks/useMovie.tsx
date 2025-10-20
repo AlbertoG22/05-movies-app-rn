73548262afbab1e3b54b307a1ccb273e680d7eea
AlbertoG22 <albertogarciamartinez99@gmail.com>
2025-10-20 12:57:33 -0600

    getMovie - use case

--- src/presentation/hooks/useMovie.tsx
+++ src/presentation/hooks/useMovie.tsx
@@ -1,17 +1,30 @@
 import React, { useEffect, useState } from 'react';
+import * as UseCases from '../../core/use-cases';
+import { movieDBFetcher } from '../../config/adapters/movieDB.adapter';
+import { FullMovie } from '../../core/entities/movie.entity';
 
 export const useMovie = ( movieId: number ) => {
   const [isLoading, setIsLoading] = useState(true);
+  const [movie, setMovie] = useState<FullMovie>();
 
   useEffect( () => {
     loadMovie();
   }, [movieId]);
 
-  const loadMovie = () => {};
+  const loadMovie = async () => {
+    setIsLoading(true);
+
+    const fullmovie = await UseCases.getMovieByIdUseCase( movieDBFetcher, movieId );
+    setMovie(fullmovie);
+    setIsLoading(false);
+
+    console.log({ fullmovie })
+  };
 
 
 
   return {
     isLoading,
+    movie,
   };
 };
--- src/presentation/screens/details/DetailsScreen.tsx
+++ src/presentation/screens/details/DetailsScreen.tsx
@@ -2,6 +2,7 @@ import { StackScreenProps } from '@react-navigation/stack';
 import React from 'react';
 import { Text, View } from 'react-native';
 import { RootStackParams } from '../../navigation/Navigation';
+import { useMovie } from '../../hooks/useMovie';
 
 interface Props extends StackScreenProps<RootStackParams, 'Details'>{
 
@@ -10,6 +11,7 @@ interface Props extends StackScreenProps<RootStackParams, 'Details'>{
 export const DetailsScreen = ({ route }: Props) => {
 
   const { movieId } = route.params;
+  const {} = useMovie( movieId );
 
   return (
     <View>
