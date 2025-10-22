96fd75a14ae6fda2903ff4435907346e5f8ea11d
AlbertoG22 <albertogarciamartinez99@gmail.com>
2025-10-21 19:43:29 -0600

    estrucutra de datos para los actores - use case

--- src/presentation/hooks/useMovie.tsx
+++ src/presentation/hooks/useMovie.tsx
@@ -2,10 +2,12 @@ import React, { useEffect, useState } from 'react';
 import * as UseCases from '../../core/use-cases';
 import { movieDBFetcher } from '../../config/adapters/movieDB.adapter';
 import { FullMovie } from '../../core/entities/movie.entity';
+import { Cast } from '../../core/entities/cast.entity';
 
 export const useMovie = ( movieId: number ) => {
   const [isLoading, setIsLoading] = useState(true);
   const [movie, setMovie] = useState<FullMovie>();
+  const [cast, setCast] = useState<Cast[]>();
 
   useEffect( () => {
     loadMovie();
@@ -14,11 +16,15 @@ export const useMovie = ( movieId: number ) => {
   const loadMovie = async () => {
     setIsLoading(true);
 
-    const fullmovie = await UseCases.getMovieByIdUseCase( movieDBFetcher, movieId );
-    setMovie(fullmovie);
-    setIsLoading(false);
+    const fullMoviePromise = UseCases.getMovieByIdUseCase( movieDBFetcher, movieId );
+    const castPromise = UseCases.getMovieCastUseCase( movieDBFetcher, movieId);
 
-    console.log({ fullmovie })
+    const [ fullMovie, cast ] = await Promise.all([ fullMoviePromise, castPromise ]);
+
+
+    setMovie(fullMovie);
+    setCast(cast);
+    setIsLoading(false);
   };
 
 
